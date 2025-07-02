62b7cb12324b2c0fbee878013d03c47721022aaa
Mikhail Kucherenko <admin@kimc.ms>
2025-07-02 14:46:38 +0700

    Task "Marked emails" completed

--- 02-basics-2/40-marked-emails/MarkedEmailsApp.js
+++ 02-basics-2/40-marked-emails/MarkedEmailsApp.js
@@ -32,19 +32,29 @@ export const emails = [
 export default defineComponent({
   name: 'MarkedEmailsApp',
 
-  setup() {},
+  setup() {
+    const query = ref(' ')
+
+    const markedEmails = computed(() => {
+      return emails.map(function(email) {
+        return (query.value && email.toLowerCase().includes(query.value.toLowerCase())) ? { marked: true, value: email } : { marked: false, value: email }
+      })
+    })
+
+    return {
+      query,
+      markedEmails
+    }
+  },
 
   template: `
     <div>
       <div class="form-group">
-        <input type="search" aria-label="Search" />
+        <input type="search" @input="query = $event.target.value" aria-label="Search for email" />
       </div>
       <ul aria-label="Emails">
-        <li>
-          Eliseo@gardner.biz
-        </li>
-        <li class="marked">
-          Jayne_Kuhic@sydney.com
+        <li v-for="email in markedEmails" :class="{ marked: email.marked }">
+          {{ email.value }}
         </li>
       </ul>
     </div>
