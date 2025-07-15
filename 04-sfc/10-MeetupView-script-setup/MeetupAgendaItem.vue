84fe93e4d7bbf696f2b68cfdf364bb8d17380c96
Mikhail Kucherenko <admin@kimc.ms>
2025-07-15 12:32:35 +0700

    Task "MeetupView script setup" completed

--- 04-sfc/10-MeetupView-script-setup/MeetupAgendaItem.vue
+++ 04-sfc/10-MeetupView-script-setup/MeetupAgendaItem.vue
@@ -1,5 +1,5 @@
-<script>
-import { computed, defineComponent } from 'vue'
+<script setup>
+import { computed } from 'vue'
 import { UiIcon } from '@shgk/vue-course-ui'
 
 const agendaItemDefaultTitles = {
@@ -24,29 +24,15 @@ const agendaItemIcons = {
   other: 'cal-sm',
 }
 
-export default defineComponent({
-  name: 'MeetupAgendaItem',
-
-  components: {
-    UiIcon,
-  },
-
-  props: {
-    agendaItem: {
-      type: Object,
-      required: true,
-    },
-  },
-
-  setup(props) {
-    const icon = computed(() => agendaItemIcons[props.agendaItem.type])
-    const title = computed(() => agendaItemDefaultTitles[props.agendaItem.type])
-    return {
-      icon,
-      title,
-    }
+const props = defineProps({
+  agendaItem: {
+    type: Object,
+    required: true,
   },
 })
+
+const icon = computed(() => agendaItemIcons[props.agendaItem.type])
+const title = computed(() => agendaItemDefaultTitles[props.agendaItem.type])
 </script>
 
 <template>
--- 04-sfc/10-MeetupView-script-setup/MeetupView.vue
+++ 04-sfc/10-MeetupView-script-setup/MeetupView.vue
@@ -1,17 +1,14 @@
-<script>
-import { defineComponent } from 'vue'
+<script setup>
 import { UiAlert, UiContainer } from '@shgk/vue-course-ui'
 import MeetupAgenda from './MeetupAgenda.vue'
 import MeetupDescription from './MeetupDescription.vue'
 import MeetupCover from './MeetupCover.vue'
 import MeetupInfo from './MeetupInfo.vue'
 
-export default defineComponent({
-  name: 'MeetupView',
-
-  components: {
-    UiAlert,
-    UiContainer,
+defineProps({
+  meetup: {
+    type: Object,
+    required: true,
   },
 })
 </script>
@@ -19,6 +16,7 @@ export default defineComponent({
 <template>
   <div>
     <!-- Обложка митапа -->
+    <MeetupCover :title="meetup.title" :image="meetup.image" />
 
     <UiContainer>
       <div class="meetup">
@@ -26,15 +24,24 @@ export default defineComponent({
           <h2>Описание</h2>
 
           <!-- Описание митапа -->
+          <MeetupDescription :description="meetup.description" />
 
           <h2>Программа</h2>
 
           <!-- Программа митапа -->
+          <MeetupAgenda v-if="meetup.agenda.length" :agenda="meetup.agenda" />
           <!-- Или при пустой программе - сообщение "Программа пока пуста..." в UiAlert -->
-          <UiAlert></UiAlert>
+          <UiAlert v-else>Программа пока пуста...</UiAlert>
+
         </div>
         <div class="meetup__aside">
+
           <!-- Краткая информация о митапе -->
+          <MeetupInfo 
+            :organizer="meetup.organizer" 
+            :place="meetup.place" 
+            :date="meetup.date" 
+          />
 
           <div class="meetup__aside-buttons"></div>
         </div>
